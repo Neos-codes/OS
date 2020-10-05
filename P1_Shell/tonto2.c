bfb9456bde685182f54948e014a8fa092dbf2fa1
rfarina97 <rodolfo.farina.2015@gmail.com>
2020-10-05 20:05:48 -0300

    procesos BG aun no listo

---
+++ P1_Shell/tonto2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int i=0;
+	while(1){
+
+		cin>>i;
+		if (i==0){
+			break;
+		}
+	}
+	cout<<"termino de tonto2\n";
+	return 0;
+}
