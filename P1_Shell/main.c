b4520489636888763ca3ac97acb7963ede45432c
floghos <caleuche94@gmail.com>
2020-10-05 21:48:03 -0400

    añadiendo registro de frecuencia de comandos

--- P1_Shell/main.c
+++ P1_Shell/main.c
@@ -4,27 +4,16 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <iostream>
+#include <vector>
+#include <string>
+
 
 using namespace std;
 
-
-/*BUGS VERSION 0.01 RODOLFO
-
-
-
-*/
-
-/*FIXED
-EXIT NOT WORKING PROPERLY
-
-*/
-
-/*ADDEd
-working CD
-
-
-*/
-
+struct entry {
+        char cmd[100];
+        int rep;
+};
 
 int stringParsing(char **cmd, char *line);
 
@@ -32,7 +21,7 @@ bool promptLine(char *line);
 
 void executeProgram(char *cmd[100]){
   pid_t pid = fork();
-    
+
   if(pid < 0){
     //printf("Error al crear hijo!\n");
     cout<<"Error al crear hijo!\n";
@@ -53,18 +42,35 @@ void executeProgram(char *cmd[100]){
   }
 }
 
+int update_freq (char *line, vector <pair<int, char *> > &command_freq) {
+    for (int i = 0; i < command_freq.size(); i++) {
+        if (strcmp(line, command_freq[i].second) == 0) {
+            command_freq[i].first++;
+            return 0;
+        }
+    }
+    pair<int, char *> aux;
+    aux.first = 1;
+    aux.second = strdup(line);
+    command_freq.push_back(aux);
+    // printf("%s check!\n", command_freq.back().second);
+    return 0;
+}
+
 
 int main(){
 
-  char line[100];
-  char *cmd[100];
-  int wordsInCmd;
-  system("clear");
+    vector <pair<int, char *> > command_freq;
+    char line[100];
+    char *cmd[100];
+    int wordsInCmd;
+    system("clear");
+    bool running = promptLine(line);
 
-  bool running = promptLine(line);
-  while (running){
-    wordsInCmd = stringParsing(cmd, line);
-    
+    while (running){
+        update_freq (line, command_freq);
+        wordsInCmd = stringParsing(cmd, line);
+
     //printf("N palabras desde main: %d\n", wordsInCmd);
     //cout<<"N palabras desde main: "<<wordsInCmd<<endl;
 
@@ -75,24 +81,31 @@ int main(){
     }*/
 
     //Check if function is built in
-    if (!strcmp(cmd[0], "exit")){
-      cout<<"Exiting, cya l8r aligator\n";
-      return 0;
+        if (!strcmp(cmd[0], "exit")){
+            cout<<"Exiting, cya l8r aligator\n";
+            return 0;
+        }
+
+        if (!strcmp(cmd[0], "cd")){
+            chdir(cmd[1]);
+            goto end;
+        }
+        if (!strcmp(cmd[0], "revisarVector")){ //for debug
+            cout << "en el vector hay " << command_freq.size() <<" elementos:" << endl;
+            for (int i = 0; i < command_freq.size(); i++) {
+                printf("%d %s\n", command_freq[i].first, command_freq[i].second);
+            }
+            goto end;
+        }
+
+
+        //Run program
+        executeProgram(cmd);
+        end:
+        running = promptLine(line);
     }
 
-    if (!strcmp(cmd[0], "cd")){
-      chdir(cmd[1]);
-      goto end;
-    }
-
-
-    //Run program
-    executeProgram(cmd);
-    end:
-    running = promptLine(line);
-  }
-  
-  return 0;
+    return 0;
 }
 
 
@@ -107,7 +120,7 @@ int stringParsing(char **cmd, char *line){
   while(line[count] != '\0'){      // Contamos palabras
     if(line[count] == ' '){
       inSpace = true;
-    } 
+    }
     else if(inSpace){
       nwords++;
       inSpace = false;
@@ -143,11 +156,11 @@ int stringParsing(char **cmd, char *line){
   for(int i = 0; i < nwords; i++){
     cout<<cmd[i]<<endl;
   }*/
-  
+
   cmd[nwords] = NULL;    // Dejamos el extra como NULL
 
 
-  
+
   //Imprimir palabras separadas
   /*printf("Palabras pareseadas\n");
   for(int i = 0; i < nwords; i++){
@@ -165,14 +178,14 @@ bool promptLine(char *line){
   char curDir[1024];
   char curUsr[64];
   char curHst[64];
-  do{                                            
+  do{
   // Vuelve a preguntar mientras
-    //printf(">>> ");                         
+    //printf(">>> ");
     // Se aprete Enter
     getcwd(curDir, sizeof(curDir));
     getlogin_r(curUsr, sizeof(curUsr));
     gethostname(curHst, sizeof(curHst));
-    cout<<"\033[1;31m"<<curUsr<<"@"<<curHst<<"\033[0m:\033[1;36m~"<<get_current_dir_name()<<" "<<getpid()<<"\033[0m > ";
+    cout << getpid() <<" \033[1;31m"<<curUsr<<"@"<<curHst<<"\033[0m:\033[1;36m~"<<get_current_dir_name()<< "\033[0m > ";
     fgets(line, 100, stdin);
   }while(line[0] == '\n');
 
