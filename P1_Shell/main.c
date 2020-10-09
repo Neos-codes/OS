6bbf12c5e8f8d99c1e5b65963821a9718c775908
rfarina97 <rodolfo.farina.2015@gmail.com>
2020-10-08 22:14:58 -0300

    minor tweaks

--- P1_Shell/main.c
+++ P1_Shell/main.c
@@ -13,60 +13,14 @@ int stringParsing(char **cmd, char *line);
 
 bool promptLine(char *line);
 
-void executeProgram(char *cmd[100], int words, bool runBg, vector<int> &activeProcesses){
-  //cout<<"Ejecuto fork()\n";
-  pid_t pid = fork();
-  if(pid < 0){
-    //printf("Error al crear hijo!\n");
-    cout<<"Error al crear hijo!\n";
-    exit(0);
-  }
-  else if(pid == 0){       // Hijo
-    //printf("Soy hijo H: %d mi padre P : %d\n", getpid(), getppid());
-    //printf("Hijo creado\n");
-    //cout<<"Hijo creado\n";
-    //char *cmds[3] = {"ls", "-n", NULL};
-    execvp(cmd[0], cmd);
-
-    //Si el comando no se reconoce se imprime el comando ejecutado
-    cout<<"No se reconoce el comando ";
-    for (int i = 0; i < words; ++i)
-    {
-      cout<<cmd[i]<<" ";
-    }
-    cout<<endl;
-
-    //Terminar el hijo
-    exit(0);
-  }
-  else{                   
-  	// Padre
-    //printf("Soy padre P: %d mi Hijo : %d\n", getpid(), pid);
-    if (!runBg){
-      wait(NULL);
-    }
-    else{
-    	//Insertamos proceso en la lista de procesos del programa
-    	activeProcesses.push_back(pid);
-    	cout<<"["<<pid<<"] in background"<<endl;
-    }
-    
-    //
-    //exit(0);
-  }
-}
+void executeProgram(char *cmd[100], int words, bool runBg, vector<int> &activeProcesses);
 
 
 int main(){
 
 
-  signal(SIGCHLD, sig_handler);
-  //sig child se produce cuado un hijo termina
-
   char line[100];
   char *cmd[100];
-
-
   vector<int> activeProcesses;
   bool running = true;
   int pidDummy;
@@ -151,6 +105,7 @@ int main(){
       }
       cout<<"Exiting\n";
       running = false;
+      continue;
     }
 
     if (!strcmp(cmd[0], "cd")){
@@ -175,7 +130,7 @@ int main(){
       cout<<"Welcome to CFF shell, it includes support for the following built-in commands"<<endl;
       cout<<"-cd [directory]\n-help\n-exit\n";
       cout<<"-------------------\n";
-      goto end;
+      continue;
     }
 
     if (!strcmp(cmd[wordsInCmd-1], "&")){
@@ -273,7 +228,7 @@ bool promptLine(char *line){
     getcwd(curDir, sizeof(curDir));
     getlogin_r(curUsr, sizeof(curUsr));
     gethostname(curHst, sizeof(curHst));
-    cout<<"\033[1;31m"<<curUsr<<"@"<<curHst<<"\033[0m:\033[1;36m~"<<get_current_dir_name()<<" "<<"\033[0m > ";
+    cout<<"["<<getpid()<<"]\033[1;31m"<<curUsr<<"@"<<curHst<<"\033[0m:\033[1;36m~"<<get_current_dir_name()<<" "<<"\033[0m> ";
     fgets(line, 100, stdin);
   }while(line[0] == '\n');
   //<<getpid()
@@ -288,37 +243,44 @@ bool promptLine(char *line){
   //printf("Prompt leyo: %s\n", line);
 }
 
-void executeProgram(char *cmd[100], int words, bool runBg, vector<int> &activeProc){
-  cout<<"Ejecuto fork()\n";
+void executeProgram(char *cmd[100], int words, bool runBg, vector<int> &activeProcesses){
+  //cout<<"Ejecuto fork()\n";
   pid_t pid = fork();
-  
-
   if(pid < 0){
     //printf("Error al crear hijo!\n");
     cout<<"Error al crear hijo!\n";
     exit(0);
   }
   else if(pid == 0){       // Hijo
-    printf("Soy hijo H: %d mi padre P : %d\n", getpid(), getppid());
+    //printf("Soy hijo H: %d mi padre P : %d\n", getpid(), getppid());
     //printf("Hijo creado\n");
     //cout<<"Hijo creado\n";
     //char *cmds[3] = {"ls", "-n", NULL};
     execvp(cmd[0], cmd);
-    //printf("Error al ejecutar comando!\n");
+
+    //Si el comando no se reconoce se imprime el comando ejecutado
     cout<<"No se reconoce el comando ";
     for (int i = 0; i < words; ++i)
     {
       cout<<cmd[i]<<" ";
     }
     cout<<endl;
+
+    //Terminar el hijo
     exit(0);
   }
-  else{                   // Padre
-    printf("Soy padre P: %d mi Hijo : %d\n", getpid(), pid);
+  else{                   
+  	// Padre
+    //printf("Soy padre P: %d mi Hijo : %d\n", getpid(), pid);
     if (!runBg){
       wait(NULL);
     }
-    activeProc.push_back(pid);
+    else{
+    	//Insertamos proceso en la lista de procesos del programa
+    	activeProcesses.push_back(pid);
+    	cout<<"["<<pid<<"] in background"<<endl;
+    }
+    
     //
     //exit(0);
   }
